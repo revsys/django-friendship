32f6db3ea10d45fbd0fdb8aa2273b35fe87f9241
Jacob Burch <jacobburch@gmail.com>
2011-12-20 01:16:03 -0800

    Template re-naming

--- friendship/views.py
+++ friendship/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from friendship.models import Friend, Follow, FriendshipRequest
 
 
-def view_friends(request, username, template_name='friendship/friends_list.html'):
+def view_friends(request, username, template_name='friendship/friend/user_list.html'):
     """ View the friends of a user """
     user = get_object_or_404(User, username=username)
     friends = Friend.objects.friends(user)
@@ -14,7 +14,7 @@ def view_friends(request, username, template_name='friendship/friends_list.html'
 
 
 @login_required
-def add_friend(request, to_username, template_name='friendship/friends_add.html'):
+def add_friend(request, to_username, template_name='friendship/friend/add.html'):
     """ Create a FriendshipRequest """
     if request.POST:
         to_user = User.objects.get(username=to_username)
@@ -59,7 +59,7 @@ def cancel_friend(request, friendship_request_id):
 
 
 @login_required
-def friendship_request_list(request, template_name='friendship/friendship_requests_list.html'):
+def friendship_request_list(request, template_name='friendship/friendship/requests_list.html'):
     """ View unread and read friendship requests """
     # friendship_requests = Friend.objects.requests(request.user)
     friendship_requests = FriendshipRequest.objects.filter(rejected__isnull=True)
@@ -67,7 +67,7 @@ def friendship_request_list(request, template_name='friendship/friendship_reques
 
 
 @login_required
-def friendship_request_list_rejected(request, template_name='friendship/friendship_requests_list.html'):
+def friendship_request_list_rejected(request, template_name='friendship/friend/requests_list.html'):
     """ View rejected friendship requests """
     # friendship_requests = Friend.objects.rejected_requests(request.user)
     friendship_requests = FriendshipRequest.objects.filter(rejected__isnull=True)
@@ -75,14 +75,14 @@ def friendship_request_list_rejected(request, template_name='friendship/friendsh
 
 
 @login_required
-def friendship_request(request, friendship_request_id, template_name='friendship/friendship_request.html'):
+def friendship_request(request, friendship_request_id, template_name='friendship/friend/request.html'):
     """ View a particular friendship request """
     f_request = get_object_or_404(FriendshipRequest, id=friendship_request_id)
 
     return render(request, template_name, {'friendship_request': f_request})
 
 
-def followers(request, username, template_name='friendship/followers_list.html'):
+def followers(request, username, template_name='friendship/follow/followers_list.html'):
     """ List this user's followers """
     user = get_object_or_404(User, username=username)
     followers = Follow.objects.followers(user)
@@ -90,7 +90,7 @@ def followers(request, username, template_name='friendship/followers_list.html')
     return render(request, template_name, {'user': user, 'followers': followers})
 
 
-def following(request, username, template_name='friendship/following_list.html'):
+def following(request, username, template_name='friendship/follow/following_list.html'):
     """ List who this user follows """
     user = get_object_or_404(User, username=username)
     following = Follow.objects.following(user)
@@ -99,7 +99,7 @@ def following(request, username, template_name='friendship/following_list.html')
 
 
 @login_required
-def add_follower(request, followee_username, template_name='friendship/following_add.html'):
+def add_follower(request, followee_username, template_name='friendship/follow/add.html'):
     """ Create a following relationship """
     if request.POST:
         followee = User.objects.get(username=followee_username)
@@ -111,7 +111,7 @@ def add_follower(request, followee_username, template_name='friendship/following
 
 
 @login_required
-def remove_follower(request, followee):
+def remove_follower(request, followee, template_name='friendship/friend/add.html'):
     """ Remove a following relationship """
     pass
 
