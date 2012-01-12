c9b994d07f65e0df2497a89eeef3dc307e1cb6dd
jefftriplett <jeff.triplett@gmail.com>
2012-01-12 14:37:05 -0600

    Adds basic friendship admins.

---
+++ friendship/admin.py
@@ -0,0 +1,25 @@
+from __future__ import absolute_import
+
+from django.contrib import admin
+
+from .models import Follow, Friend, FriendshipRequest
+
+
+class FollowAdmin(admin.ModelAdmin):
+    model = Follow
+    raw_id_fields = ('follower', 'followee')
+
+
+class FriendAdmin(admin.ModelAdmin):
+    model = Friend
+    raw_id_fields = ('to_user', 'from_user')
+
+
+class FriendshipRequestAdmin(admin.ModelAdmin):
+    model = FriendshipRequest
+    raw_id_fields = ('from_user', 'to_user')
+
+
+admin.site.register(Follow, FollowAdmin)
+admin.site.register(Friend, FriendAdmin)
+admin.site.register(FriendshipRequest, FriendshipRequestAdmin)
