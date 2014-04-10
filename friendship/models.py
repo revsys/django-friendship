98f6f7b381965c070b6ba7e229f9ea6c2dcdc6b5
Egor Yurtaev <yurtaev.egor@gmail.com>
2014-04-10 14:49:58 +0700

    Django 1.7 compatibility
    fixed: test runner
    More info https://docs.djangoproject.com/en/dev/releases/1.7/\#standalone-scripts
    
    Django 1.7 compatibility
    
    fixed .travis.yml
    
    fixed tests runner
    pip no longer supports the --use-mirrors
    
    fix: check django version
    
    fixed .travis.yml
    Add commas to a string
    
    fixed travis config
    
    fixed travis config
    
    fixed travis config

--- friendship/models.py
+++ friendship/models.py
@@ -140,7 +140,7 @@ class FriendshipManager(models.Manager):
         friends = cache.get(key)
 
         if friends is None:
-            qs = Friend.objects.select_related(depth=1).filter(to_user=user).all()
+            qs = Friend.objects.select_related('from_user', 'to_user').filter(to_user=user).all()
             friends = [u.from_user for u in qs]
             cache.set(key, friends)
 
@@ -152,7 +152,7 @@ class FriendshipManager(models.Manager):
         requests = cache.get(key)
 
         if requests is None:
-            qs = FriendshipRequest.objects.select_related(depth=1).filter(
+            qs = FriendshipRequest.objects.select_related('from_user', 'to_user').filter(
                 to_user=user).all()
             requests = list(qs)
             cache.set(key, requests)
@@ -165,7 +165,7 @@ class FriendshipManager(models.Manager):
         requests = cache.get(key)
 
         if requests is None:
-            qs = FriendshipRequest.objects.select_related(depth=1).filter(
+            qs = FriendshipRequest.objects.select_related('from_user', 'to_user').filter(
                     from_user=user).all()
             requests = list(qs)
             cache.set(key, requests)
@@ -178,7 +178,7 @@ class FriendshipManager(models.Manager):
         unread_requests = cache.get(key)
 
         if unread_requests is None:
-            qs = FriendshipRequest.objects.select_related(depth=1).filter(
+            qs = FriendshipRequest.objects.select_related('from_user', 'to_user').filter(
                 to_user=user,
                 viewed__isnull=True).all()
             unread_requests = list(qs)
@@ -192,7 +192,7 @@ class FriendshipManager(models.Manager):
         count = cache.get(key)
 
         if count is None:
-            count = FriendshipRequest.objects.select_related(depth=1).filter(
+            count = FriendshipRequest.objects.select_related('from_user', 'to_user').filter(
                 to_user=user,
                 viewed__isnull=True).count()
             cache.set(key, count)
@@ -205,7 +205,7 @@ class FriendshipManager(models.Manager):
         read_requests = cache.get(key)
 
         if read_requests is None:
-            qs = FriendshipRequest.objects.select_related(depth=1).filter(
+            qs = FriendshipRequest.objects.select_related('from_user', 'to_user').filter(
                 to_user=user,
                 viewed__isnull=False).all()
             read_requests = list(qs)
@@ -219,7 +219,7 @@ class FriendshipManager(models.Manager):
         rejected_requests = cache.get(key)
 
         if rejected_requests is None:
-            qs = FriendshipRequest.objects.select_related(depth=1).filter(
+            qs = FriendshipRequest.objects.select_related('from_user', 'to_user').filter(
                 to_user=user,
                 rejected__isnull=False).all()
             rejected_requests = list(qs)
