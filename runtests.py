4c618888174a53df5cfa83c54f98dc5952fa2574
Jeff Triplett <jeff.triplett@gmail.com>
2014-10-06 23:43:23 -0500

    Updated runtests

--- runtests.py
+++ runtests.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python
 import sys
-
 import django
-from django.conf import settings
-from django.utils import six
-from django.conf import global_settings
+
+from django.conf import global_settings, settings
+
 
 settings.configure(
     DATABASES={
@@ -18,10 +17,7 @@ settings.configure(
         'friendship.tests',
     ],
     ROOT_URLCONF='friendship.urls',
-    TEST_RUNNER=('django_coverage.coverage_runner.CoverageRunner' if six.PY2
-                 else 'django.test.runner.DiscoverRunner' if django.VERSION[0:2] >= (1, 6)
-                 else 'django.test.simple.DjangoTestSuiteRunner'),
-    MIDDLEWARE_CLASSES = global_settings.MIDDLEWARE_CLASSES + (
+    MIDDLEWARE_CLASSES=global_settings.MIDDLEWARE_CLASSES + (
         'django.contrib.sessions.middleware.SessionMiddleware',
         'django.contrib.auth.middleware.AuthenticationMiddleware',)
 )
@@ -29,12 +25,15 @@ settings.configure(
 
 def runtests(*test_args):
     import django.test.utils
+
     if django.VERSION[0:2] >= (1, 7):
         django.setup()
+
     runner_class = django.test.utils.get_runner(settings)
-    test_runner = runner_class(verbosity=1, interactive=True)
+    test_runner = runner_class(verbosity=1, interactive=True, failfast=False)
     failures = test_runner.run_tests(['friendship'])
     sys.exit(failures)
+
 
 if __name__ == '__main__':
     runtests()
