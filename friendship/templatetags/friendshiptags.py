5d323401066a4a56b508d1643b7dcf7042cdd93a
Peter Bassem Sobhi <peterbsobhi@gmail.com>
2018-06-06 11:25:05 +0200

    Change assignment_tag to simple_tag

--- friendship/templatetags/friendshiptags.py
+++ friendship/templatetags/friendshiptags.py
@@ -5,7 +5,7 @@ from friendship.models import Friend, Follow, FriendshipRequest, Block
 register = template.Library()
 
 
-@register.assignment_tag(takes_context=True)
+@register.simple_tag(takes_context=True)
 def get_by_name(context, name):
     """Tag to lookup a variable in the current context."""
     return context[name]
