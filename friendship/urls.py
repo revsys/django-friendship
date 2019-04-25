3aa8f8db4e3510e0bcc6251a41b0078bfbc658d4
Uri <uri@speedy.net>
2019-04-25 21:02:32 +0300

    Reformat code - add spaces.

--- friendship/urls.py
+++ friendship/urls.py
@@ -4,8 +4,8 @@ except ImportError:
     from django.conf.urls.defaults import url
 from friendship.views import view_friends, friendship_add_friend, friendship_accept, \
     friendship_reject, friendship_cancel, friendship_request_list, \
-    friendship_request_list_rejected, friendship_requests_detail, followers,\
-    following, follower_add, follower_remove, all_users,block_add,block_remove,blockers,blocking
+    friendship_request_list_rejected, friendship_requests_detail, followers, \
+    following, follower_add, follower_remove, all_users, block_add, block_remove, blockers, blocking
 
 urlpatterns = [
     url(
