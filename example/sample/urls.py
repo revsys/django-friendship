b72c9d7625c5fefac3bc1027f0f79380c7e3bf33
Jeff Triplett <jeff.triplett@gmail.com>
2021-08-25 18:57:54 -0500

    :arrow_up: Drop non-LTS Django support

--- example/sample/urls.py
+++ example/sample/urls.py
@@ -1,10 +1,11 @@
-from django.conf.urls import include, url
+from django.conf.urls import include
 from django.contrib import admin
+from django.urls import path
 
 urlpatterns = [
     # Examples:
     # url(r'^$', 'sample.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r"^friendship/", include("friendship.urls")),
-    url(r"^admin/", admin.site.urls),
+    path('friendship/', include("friendship.urls")),
+    path('admin/', admin.site.urls),
 ]
--- friendship/urls.py
+++ friendship/urls.py
@@ -1,7 +1,4 @@
-try:
-    from django.conf.urls import url
-except ImportError:
-    from django.conf.urls.defaults import url
+from django.urls import path
 from friendship.views import (
     all_users,
     block_add,
@@ -23,85 +20,53 @@ from friendship.views import (
 )
 
 urlpatterns = [
-    url(regex=r"^users/$", view=all_users, name="friendship_view_users"),
-    url(
-        regex=r"^friends/(?P<username>[\w-]+)/$",
-        view=view_friends,
+    path('users/', view=all_users, name="friendship_view_users"),
+    path('friends/<slug:username>/', view=view_friends,
         name="friendship_view_friends",
     ),
-    url(
-        regex=r"^friend/add/(?P<to_username>[\w-]+)/$",
-        view=friendship_add_friend,
+    path('friend/add/<slug:to_username>/', view=friendship_add_friend,
         name="friendship_add_friend",
     ),
-    url(
-        regex=r"^friend/accept/(?P<friendship_request_id>\d+)/$",
-        view=friendship_accept,
+    path('friend/accept/<int:friendship_request_id>/', view=friendship_accept,
         name="friendship_accept",
     ),
-    url(
-        regex=r"^friend/reject/(?P<friendship_request_id>\d+)/$",
-        view=friendship_reject,
+    path('friend/reject/<int:friendship_request_id>/', view=friendship_reject,
         name="friendship_reject",
     ),
-    url(
-        regex=r"^friend/cancel/(?P<friendship_request_id>\d+)/$",
-        view=friendship_cancel,
+    path('friend/cancel/<int:friendship_request_id>/', view=friendship_cancel,
         name="friendship_cancel",
     ),
-    url(
-        regex=r"^friend/requests/$",
-        view=friendship_request_list,
+    path('friend/requests/', view=friendship_request_list,
         name="friendship_request_list",
     ),
-    url(
-        regex=r"^friend/requests/rejected/$",
-        view=friendship_request_list_rejected,
+    path('friend/requests/rejected/', view=friendship_request_list_rejected,
         name="friendship_requests_rejected",
     ),
-    url(
-        regex=r"^friend/request/(?P<friendship_request_id>\d+)/$",
-        view=friendship_requests_detail,
+    path('friend/request/<int:friendship_request_id>/', view=friendship_requests_detail,
         name="friendship_requests_detail",
     ),
-    url(
-        regex=r"^followers/(?P<username>[\w-]+)/$",
-        view=followers,
+    path('followers/<slug:username>/', view=followers,
         name="friendship_followers",
     ),
-    url(
-        regex=r"^following/(?P<username>[\w-]+)/$",
-        view=following,
+    path('following/<slug:username>/', view=following,
         name="friendship_following",
     ),
-    url(
-        regex=r"^follower/add/(?P<followee_username>[\w-]+)/$",
-        view=follower_add,
+    path('follower/add/<slug:followee_username>/', view=follower_add,
         name="follower_add",
     ),
-    url(
-        regex=r"^follower/remove/(?P<followee_username>[\w-]+)/$",
-        view=follower_remove,
+    path('follower/remove/<slug:followee_username>/', view=follower_remove,
         name="follower_remove",
     ),
-    url(
-        regex=r"^blockers/(?P<username>[\w-]+)/$",
-        view=blockers,
+    path('blockers/<slug:username>/', view=blockers,
         name="friendship_blockers",
     ),
-    url(
-        regex=r"^blocking/(?P<username>[\w-]+)/$",
-        view=blocking,
+    path('blocking/<slug:username>/', view=blocking,
         name="friendship_blocking",
     ),
-    url(
-        regex=r"^block/add/(?P<blocked_username>[\w-]+)/$",
-        view=block_add,
+    path('block/add/<slug:blocked_username>/', view=block_add,
         name="block_add",
     ),
-    url(
-        regex=r"^block/remove/(?P<blocked_username>[\w-]+)/$",
-        view=block_remove,
+    path('block/remove/<slug:blocked_username>/', view=block_remove,
         name="block_remove",
     ),
 ]
