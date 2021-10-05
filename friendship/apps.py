7139b0785eeaaf6e67b49458aadd814a34db2a49
Uri <uri@speedy.net>
2021-10-05 17:20:36 +0300

    AppConfig: Changed default_auto_field to "django.db.models.BigAutoField" + added migrations.
    (Support more than 2**31 records in all models).

--- friendship/apps.py
+++ friendship/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class FriendshipConfig(AppConfig):
-    default_auto_field = "django.db.models.AutoField"
+    default_auto_field = "django.db.models.BigAutoField"
     name = "friendship"
