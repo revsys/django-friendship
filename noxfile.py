8ad1fd87347819b7d8ef339ae1b631bc14ece21c
Jeff Triplett <jefftriplett@users.noreply.github.com>
2023-09-27 18:26:18 -0500

    :arrow_up: Upgrades Django to 4.2 (#186)
    * :arrow_up: Upgrades Django to 4.2
    
    * :tractor: Refactor nox testing
    
    * :gear: Cleans up testing matrix
    
    * :fire: Drop pinned requirements.txt
    
    We don't use these anyways. :shrug:
    
    * :gear: Adds unpinned deps
    
    ---------
    
    Co-authored-by: Jeffrey Triplett <jefftriplett@mbp-2019.local>

--- noxfile.py
+++ noxfile.py
@@ -1,26 +1,29 @@
 import nox
 
-DJANGO_VERSIONS = ["2.2", "3.2", "4.0", "4.1"]
-PYTHON_VERSIONS = ["3.6", "3.7", "3.8", "3.9", "3.10", "3.11"]
 
-INVALID_PYTHON_DJANGO_SESSIONS = [
-    ("3.6", "4.0"),
-    ("3.6", "4.1"),
-    ("3.6", "4.2"),
-    ("3.7", "4.0"),
-    ("3.7", "4.1"),
-    ("3.7", "4.2"),
-    ("3.11", "3.2"),
-    # ("3.11", "4.0"),
-    # ("3.11", "4.1"),
-]
+DJANGO_STABLE_VERSION = ["4.2"]
+DJANGO_VERSIONS = ["3.2", "4.1", "main"]
+
+PYTHON_STABLE_VERSION = ["3.11"]
+PYTHON_VERSIONS = ["3.8", "3.9", "3.10"]
 
 
-@nox.session(python=PYTHON_VERSIONS, tags=["django"])
+@nox.session(python=PYTHON_STABLE_VERSION, tags=["django"])
 @nox.parametrize("django", DJANGO_VERSIONS)
-def tests(session: nox.Session, django: str) -> None:
-    if (session.python, django) in INVALID_PYTHON_DJANGO_SESSIONS:
-        session.skip()
+def test_django_version(session: nox.Session, django: str) -> None:
+    session.install(".[test]")
+
+    if django == "main":
+        session.install("https://github.com/django/django/archive/refs/heads/main.zip")
+    else:
+        session.install(f"django~={django}")
+
+    session.run("pytest", *session.posargs)
+
+
+@nox.session(python=PYTHON_VERSIONS, tags=["python"])
+@nox.parametrize("django", DJANGO_STABLE_VERSION)
+def test_python_version(session: nox.Session, django: str) -> None:
     session.install(".[test]")
     session.install(f"django~={django}")
     session.run("pytest", *session.posargs)
